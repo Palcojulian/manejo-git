f2b22ca68fdc63f9ac37cad00483bbd6261f6605
Julian <palcojulian@outlook.com>
2022-08-30 22:50:31 -0500

    Proyecto con java

---
+++ src/paquete1/Main.java
@@ -0,0 +1,12 @@
+package paquete1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        int num1 = 4;
+        int num2 = 5;
+        
+        System.out.println("La suma de " + (num1 + num2));
+        
+    }
+}
